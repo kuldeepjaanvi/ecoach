06f35f02bf55b6646b2ff1a59a60c0fd2a5bfe92
Kuldeep Tamrakar <kuldeep844ever@gmail.com>
2020-05-08 15:07:26 +0530

    intial commet with local json

---
+++ app/api.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+import * as c from './constants';
+import data from './ecoach.json'
+
+export async function getHeadlines(country = "us"){
+    try{
+        // let requests = [];
+        // c.CATEGORIES.map((category) => {
+        //     let url =  `${c.HEADLINES}&country=${country}&category=${category.toLowerCase()}`;
+        //     requests.push(axios.get(url))
+        // });
+
+        // let response = await Promise.all(requests);
+        // response.map((resp, idx) => {
+        //     let {articles, totalResults} = resp.data;
+
+        //     response[idx] = {articles, totalResults};
+        // });
+        let {articles, totalResults} = data;
+
+       // response[0] = {articles, totalResults};
+        let [technology] = [{articles, totalResults}];
+
+        return {technology};
+
+    }catch (e) {
+        throw new Error(e);
+    }
+}
+
+export async function getHeadlinesByCategory(category, page=1, country = "us"){
+    try{
+        const url = `${c.HEADLINES}&category=${category}&page=${page}&country=${country}`;
+        let res = await axios.get(url);
+
+        return res.data;
+    }catch (e) {
+        throw new Error(e);
+    }
+}
+
+export async function search(query, cancelToken){
+    try{
+        const url = `${c.SEARCH}&q=${query.toLowerCase()}`;
+        let res = await axios.get(url, {
+            cancelToken: cancelToken.token,
+        });
+
+        return res.data;
+
+    }catch (error) {
+        let err = new Error(error.message);
+        err.isCancel = (axios.isCancel(error));
+
+        throw err;
+    }
+}
